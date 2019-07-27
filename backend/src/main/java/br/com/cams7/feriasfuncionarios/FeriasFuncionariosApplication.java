e177d7c336e22e7ec17b9a4a9de5fed44c72e9a3
César Magalhães <ceanma@gmail.com>
2019-07-27 09:42:13 -0300

    Backend da aplicação "Férias Funcionários"

---
+++ backend/src/main/java/br/com/cams7/feriasfuncionarios/FeriasFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.cams7.feriasfuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriasFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeriasFuncionariosApplication.class, args);
+	}
+
+}
